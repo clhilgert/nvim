4f733553416f82c0aa42ef0ec51018348e10d065
Clay Hilgert <claytonhilgertmst@gmail.com>
2024-12-05 17:30:12 -0600

    create lua ftplugin config and update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,10 +26,11 @@ require("lazy").setup({
   spec = {
     -- import your plugins
 	{ import = "config.plugins" },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
